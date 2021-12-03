0a0fdfdf7ea78a53cd9bc83110d6750affebe4ed
Rielch <2288@holbertonschool.com>
2021-12-03 02:26:01 +0000

    Add part 11

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Returns the transpose of a numpy.ndarray"""
+
+
+def np_transpose(matrix):
+    """Returns the transpose of a numpy.ndarray using ndarray.transpose"""
+    return matrix.transpose()
