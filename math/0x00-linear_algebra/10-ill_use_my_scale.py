3a6a14e95f77eda89952e242e41364fdf720bc6f
Rielch <2288@holbertonschool.com>
2021-12-03 02:13:48 +0000

    Add part 10

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Returns the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """Returns the shape of a numpy.ndarray using ndarray.shape"""
+    return matrix.shape
