8b1fb8d27bdedd7fcc699a75e26ce5992c1c76e2
Rielch <2288@holbertonschool.com>
2021-12-03 02:49:58 +0000

    Fix part 12

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Performs element-wise addition, substraction, multiplication and division"""
-import numpy as np
 
 
 def np_elementwise(mat1, mat2):
     """Performs addition, substraction, multiplication and division"""
+    import numpy as np
     return (np.add(mat1, mat2), np.subtract(mat1, mat2),
             np.multiply(mat1, mat2), np.divide(mat1, mat2))
