31dcba01eddc9bea2e5852ae87a00148bc86b156
Rielch <2288@holbertonschool.com>
2021-12-03 00:04:31 +0000

    Add part 6

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """Creates a new array and concatenates the two arrays in it"""
+
+    new_array = list(arr1)
+
+    for element in arr2:
+        new_array.append(element)
+
+    return new_array
