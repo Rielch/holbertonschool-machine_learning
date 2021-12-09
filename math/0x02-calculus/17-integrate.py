446b6a6067db99fdcd1ae40e4c67ad36c0abd1da
Rielch <2288@holbertonschool.com>
2021-12-09 23:52:55 +0000

    Add parts 11 to 17

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Write a function that calculates the integral of a polynomial:"""
+
+
+def poly_integral(poly, C=0):
+    """ function that calculates the integral of a polynomial:"""
+
+    if type(poly) is not list or not poly or len(poly) == 0:
+        return None
+
+    elif type(C) is not int:
+        return None
+
+    elif poly == [0]:
+        return [C]
+
+    result = []
+
+    for x in range(len(poly)):
+        result.append(poly[x] / (x + 1))
+
+    return result
+    
