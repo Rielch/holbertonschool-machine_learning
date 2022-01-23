c12810de93e0454f45ddc7f2a70bb30c749efa3e
Rielch <2288@holbertonschool.com>
2022-01-23 19:18:30 +0000

    Fix part 4

--- supervised_learning/0x04-error_analysis/4-f1_score.py
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -5,7 +5,8 @@ import numpy as np
 
 def f1_score(confusion):
     """Calculates the F1 score of a confusion matrix"""
-    tp = np.diagonal(confusion)
-    fp = np.sum(confusion, axis=0) - tp
-    fn = np.sum(confusion, axis=1) - tp
-    return tp / (tp + 0.5 * (fp + fn))
+    sensitivity = __import__('1-sensitivity').sensitivity
+    precision = __import__('2-precision').precision
+    rec = sensitivity(confusion)
+    prec = precision(confusion)
+    return (2 * rec * prec) / (prec + rec)
