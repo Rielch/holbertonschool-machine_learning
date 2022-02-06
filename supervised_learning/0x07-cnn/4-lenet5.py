b805194d769b1d1a6016ac162696d01e42c0bf22
Rielch <2288@holbertonschool.com>
2022-02-06 17:13:08 +0000

    Fix part 4

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -15,16 +15,16 @@ def lenet5(x, y):
                                padding="same",
                                activation='relu')
     L2_pool = tf.layers.MaxPooling2D(inputs=L1_conv,
-                                      pool_size=(2, 2),
-                                      strides=(2, 2))
+                                     pool_size=(2, 2),
+                                     strides=(2, 2))
     L3_conv = tf.layers.Conv2D(inputs=L2_pool,
                                filters=16,
                                kernel_size=(5, 5),
                                kernel_initializer=kernel,
                                activation='relu')
     L4_pool = tf.layers.MaxPooling2D(inputs=L3_conv,
-                                      pool_size=(2, 2),
-                                      strides=(2, 2))
+                                     pool_size=(2, 2),
+                                     strides=(2, 2))
     L4_flat = tf.layers.flatten(L4_pool)
     L5_fc = tf.layers.Dense(inputs=L4_flat,
                             units=120,
@@ -37,9 +37,10 @@ def lenet5(x, y):
     L7_fc = tf.layers.Dense(inputs=L6_fc,
                             units=10,
                             kernel_initializer=kernel,)
-    Y_pred = tf.nn.softmax(L7_fc)
+    Y_pred = tf.math.softmax(L7_fc)
     loss = tf.losses.softmax_cross_entropy(y, L7_fc)
     Train_op = tf.train.AdamOptimizer().minimize(loss)
-    acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(L7_fc, axis=1),
-                                          tf.argmax(y, axis=1)), "float32"))
+    acc = tf.reduce_mean(tf.to_float(tf.assert_equal(tf.argmax(L7_fc, axis=1),
+                                                     tf.argmax(y, axis=1)),
+                                     name="ToFloat"))
     return Y_pred, Train_op, loss, acc
