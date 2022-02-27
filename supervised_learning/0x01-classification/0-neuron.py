0b7eed28029c4d174b56fa77cfcce48d2c70da41
Rielch <2288@holbertonschool.com>
2022-02-27 17:34:43 -0300

    Redo parts 0 and 1

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -7,14 +7,11 @@ class Neuron:
     """Neuron that performs binary classification"""
 
     def __init__(self, nx):
-        """Initializates the neuron"""
-
+        """Initializes the neuron"""
         if type(nx) is not int:
-            raise TypeError('nx must be an integer')
-
+            raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError('nx must be a positive integer')
-
+            raise ValueError("nx must be a positive integer")
         self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
--- supervised_learning/0x01-classification/1-neuron.py
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -7,29 +7,25 @@ class Neuron:
     """Neuron that performs binary classification"""
 
     def __init__(self, nx):
-        """Initializates the neuron"""
-
+        """Initializes the neuron"""
         if type(nx) is not int:
-            raise TypeError('nx must be an integer')
-
+            raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError('nx must be a positive integer')
-
+            raise ValueError("nx must be a positive integer")
         self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
 
     @property
     def W(self):
-        """Gets the value of W"""
+        """Getter for the weights of the neuron"""
         return self.__W
 
     @property
     def b(self):
-        """Gets the value of b"""
+        """Getter for the bias of the neuron"""
         return self.__b
 
     @property
     def A(self):
-        """Gets the value of A"""
-        return self.__A
+        """Getter for the activation of the neuron"""
