7dd585e8ddf0e37e9660cb632560e5212a8bd355
Rielch <2288@holbertonschool.com>
2022-02-27 17:55:16 -0300

    Redo part 2

--- supervised_learning/0x01-classification/2-neuron.py
+++ supervised_learning/0x01-classification/2-neuron.py
@@ -7,36 +7,33 @@ class Neuron:
     """Neuron that performs binary classification"""
 
     def __init__(self, nx):
-        """Initializates the neuron"""
-
+        """Initializes the neuron"""
         if type(nx) is not int:
-            raise TypeError('nx must be an integer')
-
+            raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError('nx must be a positive integer')
-
+            raise ValueError("nx must be a positive integer")
         self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
 
     @property
     def W(self):
-        """Gets the value of W"""
+        """Getter for the weights of the neuron"""
         return self.__W
 
     @property
     def b(self):
-        """Gets the value of b"""
+        """Getter for the bias of the neuron"""
         return self.__b
 
     @property
     def A(self):
-        """Gets the value of A"""
+        """Getter for the activation of the neuron"""
         return self.__A
 
     def forward_prop(self, X):
-        """Calculates the forward propagation"""
-        forward_prop = np.matmul(self.__W, X) + self.__b
-        sigmoid = 1 / (1 + (np.e**(-forward_prop)))
+        """Calculates the forward propagation of a neuron"""
+        forward = np.matmul(self.__W, X) + self.__b
+        sigmoid = 1 / (1 + (np.e ** (-forward)))
         self.__A = sigmoid
         return self.__A
