e4d5afb039050a22180202c858581dd6701326ec
Rielch <2288@holbertonschool.com>
2022-03-26 16:22:23 -0300

    Fix part 3

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Function that trains a loaded neural network
  model using mini-batch gradient descent"""
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
